9fe5259e57a8f4a47af3c79a74bbc43c08532111
Sanidhyanegi07 <sanidhyanegi050107@gmail.com>
2026-08-23 22:53:49 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>s1=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+            {
+                s1.push(c);
+            }
+            else
+            {
+                if(s1.isEmpty())
+                {
+                    return false;
+                }
+                char top=s1.pop();
+                if(c==')'&&top!='(' || c=='}'&&top!='{' || c==']'&&top!='[')
+                {
+                    return false;
+                }
+            }
+        }
+        return s1.isEmpty();
+    }
+}
